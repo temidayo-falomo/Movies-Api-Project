9544d82aaa6c2213862309280b0db04288dc302d
Temidayo Falomo <temmideee@gmail.com>
2022-05-13 19:30:11 +0100

    Update InfoBox.jsx

--- src/components/rightBar/infoBox/InfoBox.jsx
+++ src/components/rightBar/infoBox/InfoBox.jsx
@@ -5,11 +5,11 @@ function InfoBox(props) {
   return (
     <div className="info-box">
       <div className="top-info-box">
-        <img src={props.contd.image} alt="ff" className="wv" />
+        <img src={props.contd.image} alt="img" className="wv" />
         <div>
           <h4>{props.contd.title}</h4>
-          <p>{props.contd.left} Episode left</p>
-          <div className="prog-bar"><img src={props.contd.progress} alt="" /></div>
+          <p>{props.contd.left} Episodes left</p>
+          <div className="prog-bar"><img src={props.contd.progress} alt="img" /></div>
         </div>
       </div>
       <div className="info-box-btns">
